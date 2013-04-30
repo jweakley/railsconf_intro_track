f66e352f5728f85eb6568fbe90910c23010b788b
Joe Weakley <joe.weakley@digitalaugment.com>
2013-04-30 12:11:43 -0700

    Added the ability to complete a task. Add passing test to complete task.
    Extract method to complete task.
    
    Extracted complete task to a method.
    
    Formatting fix.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TddIntro::Application.routes.draw do
-  resources :tasks, only: [:index, :new, :create]
+  resources :tasks, only: [:index, :new, :create] do
+    resources :completions, only: [:create]
+  end
   root to: 'tasks#index'
 end
